59c2ee34d0a4d6370ca06e12e259be22d0ad4e69
NeRF-MIR <308334804@qq.com>
2024-12-28 16:05:46 +0800

    code of NeRF-MIR

---
+++ load_blender.py
@@ -0,0 +1,89 @@
+import os
+import torch
+import numpy as np
+import imageio 
+import json
+import torch.nn.functional as F
+import cv2
+
+
+trans_t = lambda t : torch.Tensor([
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,t],
+    [0,0,0,1]]).float()
+
+rot_phi = lambda phi : torch.Tensor([
+    [1,0,0,0],
+    [0,np.cos(phi),-np.sin(phi),0],
+    [0,np.sin(phi), np.cos(phi),0],
+    [0,0,0,1]]).float()
+
+rot_theta = lambda th : torch.Tensor([
+    [np.cos(th),0,-np.sin(th),0],
+    [0,1,0,0],
+    [np.sin(th),0, np.cos(th),0],
+    [0,0,0,1]]).float()
+
+
+def pose_spherical(theta, phi, radius):
+    c2w = trans_t(radius)
+    c2w = rot_phi(phi/180.*np.pi) @ c2w
+    c2w = rot_theta(theta/180.*np.pi) @ c2w
+    c2w = torch.Tensor(np.array([[-1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]])) @ c2w
+    return c2w
+
+
+def load_blender_data(basedir, half_res=False):
+    splits = ['train']
+    metas = {}
+    for s in splits:
+        with open(os.path.join(basedir, 'transforms_{}.json'.format(s)), 'r') as fp:
+            metas[s] = json.load(fp)
+
+    all_imgs = []
+    all_poses = []
+    for s in splits:
+        meta = metas[s]
+        imgs = []
+        poses = []
+        for frame in meta['frames']:
+            fname = os.path.join(basedir, frame['file_path'] + '.png')
+
+            imgs.append(imageio.imread(fname))
+            poses.append(np.array(frame['transform_matrix']))
+        imgs = (np.array(imgs) / 255.).astype(np.float32)
+        poses = np.array(poses).astype(np.float32)  # [n, 4, 4]
+
+        all_imgs.append(imgs)
+        all_poses.append(poses)
+    imgs = np.concatenate(all_imgs, 0) # [N, H, W, 4]
+    poses = np.concatenate(all_poses, 0)
+
+    
+    H, W = imgs[0].shape[:2]
+    camera_angle_x = float(meta['camera_angle_x'])
+    focal = .5 * W / np.tan(.5 * camera_angle_x)
+    render_poses = torch.stack([pose_spherical(angle, -30.0, 4.0) for angle in np.linspace(-180,180,40+1)[:-1]], 0)
+
+
+
+    if half_res:
+        H = H//2
+        W = W//2
+        focal = focal/2.
+
+        imgs_half_res = np.zeros((imgs.shape[0], H, W, 4))
+        for i, img in enumerate(imgs):
+            imgs_half_res[i] = cv2.resize(img, (W, H), interpolation=cv2.INTER_AREA)
+
+
+        imgs = imgs_half_res
+
+    patch_matrix = np.loadtxt(basedir +'/train/position.txt')
+
+
+
+    return imgs, poses, render_poses, [H, W, focal], [i for i in range(0,100)], patch_matrix
+
+
